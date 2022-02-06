bdbf1e7318f4c94d36792d357b544e05c04b865f
owllxz <lsepulvedam99@gmail.com>
2022-02-06 14:38:02 -0300

    Tienda UI

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -2,6 +2,7 @@ module Api
     module V1
         class ProductsController < ApplicationController
             def index
+                #Obtener todos los productos de la base de datos
                 products = Product.all
                 render json: {status: 'SUCCESS', message: 'Loaded products', data: products}, status: :ok
             end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  #API V1 - Categoria y Productos
   namespace 'api' do
     namespace 'v1' do
       resources :products
