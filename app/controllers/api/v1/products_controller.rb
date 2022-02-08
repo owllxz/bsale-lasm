45c68ebdfbd673069ee6bfbd7c7c5ca4b855189e
owllxz <lsepulvedam99@gmail.com>
2022-02-08 17:46:25 -0300

    Search Rails(In Show Route) (local db)

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -6,6 +6,16 @@ module Api
                 products = Product.all
                 render json: {status: 'SUCCESS', message: 'Loaded products', data: products}, status: :ok
             end
+            def show
+                if params[:id].blank?
+                    products = Product.all
+                    render json: {status: 'SUCCESS', message: 'Loaded products', data: products}, status: :ok
+                else
+                    parametro = params[:id].downcase #Dejamos en minuscula
+                    products = Product.all.where("lower(name) LIKE ?", "%#{parametro}%") #Emparejamos con el parametro recibido
+                    render json: {status: parametro, message: 'Loaded products', data: products}, status: :ok
+                end
+            end
         end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   #API V1 - Categoria y Productos
   namespace 'api' do
     namespace 'v1' do
-      resources :products
+      resources :categorys
     end
   end
   namespace 'api' do
     namespace 'v1' do
-      resources :categorys
+      resources :products
     end
   end
 end
